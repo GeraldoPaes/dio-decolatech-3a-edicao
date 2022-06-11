67e15588e8d4756373c454c1d11a0bd56fe43443
Geraldo Fernando <gfernandopaes@gmail.com>
2022-06-11 16:18:32 -0300

    Adicionada atividade do Curso: Primeiros passos para desenvolvimento web

---
+++ projeto-web-intro/accordion.php
@@ -0,0 +1,50 @@
+<html>
+
+    <head>
+        <title>Meu Accordion (?) em PHP</title>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+        <script>
+            $(function() {
+                $( "#accordion" ).accordion();
+            } );
+        </script>
+    </head>
+
+    <body>
+
+        <div id="accordion">
+            <h3>FUMES</h3>
+            <div>
+                <p>
+                    The morning light shines a lifeline<br>
+                    Escape is what I need<br>
+                    I should've listened last night, girl<br>
+                    This is beyond belief<br>
+                    That we keep falling for the lure of<br>
+                    Liquid confidence and lies<br>
+                    And we, skydive from walls we built<br>
+                    Dying just to feel alive<br>
+                    On the way down screaming<br>
+                </p>
+            </div>
+            <h3>Chorus</h3>
+            <div>
+                <p>
+                    Woah, oh, oh<br>
+                    If all we have is time, then we'll be alright<br>
+                    Woah, oh, oh<br>
+                    It's not much, but it's better than nothing<br>
+                    Woah, oh, oh<br>
+                    We're running on fumes, but we'll make it through the night<br>
+                    Woah, oh, oh<br>
+                    It's not love, but it's better than dreaming<br>
+                </p>
+            </div>
+        </div>
+
+    </body>
+
+</html>
